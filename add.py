c4fd36380642eebf5a417672044f1551830c49b3
kaveen <kaveenmp@gmail.com>
2021-02-19 13:48:03 +0530

    initial commit

---
+++ add.py
@@ -0,0 +1,17 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=10
+>>> b=45
+>>> c=a+b
+>>> print("c")
+c
+>>> a=10
+>>> b=45
+>>> c=a+b
+SyntaxError: multiple statements found while compiling a single statement
+>>> a=10
+>>> b=20
+>>> c=a+b
+>>> print(c)
+30
+>>> 
